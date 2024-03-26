00fcb7003c1861bd42857fee6213a69cc05002a3
icad08 <irsyadrm08@gmail.com>
2024-03-26 14:27:10 +0700

    input data 1

--- fungsiSwitch.cpp
+++ fungsiSwitch.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int bilangan1, bilangan2;
 void inputData(){
+    cout << "Masukkan bilangan pertama:" ;
+    cin >> bilangan1;
+    cout << "Masukan bilangan kedua:" ;
+    cin >> bilangan2;
 }
+
 
 int main (){
 
